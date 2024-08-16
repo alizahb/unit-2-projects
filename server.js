88c88ea1f269b88439d08670fc2c84c03c35b741
Alizah <alizahvberg@gmail.com>
2024-08-16 17:46:55 -0400

    views

--- server.js
+++ server.js
@@ -7,43 +7,47 @@ const methodOverride = require('method-override');
 const morgan = require('morgan');
 const session = require('express-session');
 
-//const isSignedIn = require('./middleware/is-signed-in.js');
-//const passUserToView = require('./middleware/pass-user-to-view.js');
-//const controllers here
+const isSignedIn = require('./middleware/is-signed-in.js');
+const passUserToView = require('./middleware/pass-user-to-view.js');
+const authController = require('./controllers/auth.js');
+const usersController = require('./controllers/users.js');
+const itemsController = require('./controllers/items.js');
+const tripsController = require("./controllers/trips.js");
+const listsController = require("./controllers/lists.js");
 
 const port = process.env.PORT ? process.env.PORT : '3000';
 
 mongoose.connect(process.env.MONGODB_URI);
 
 mongoose.connection.on('connected', () => {
-    console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
+  console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
+});
+
+app.use(express.urlencoded({ extended: false }));
+app.use(methodOverride('_method'));
+// app.use(morgan('dev'));
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+
+app.get('/', (req, res) => {
+  res.render('index.ejs', {
+    user: req.session.user,
   });
-  
-  app.use(express.urlencoded({ extended: false }));
-  app.use(methodOverride('_method'));
-  // app.use(morgan('dev'));
-  app.use(
-    session({
-      secret: process.env.SESSION_SECRET,
-      resave: false,
-      saveUninitialized: true,
-    })
-  );
+});
 
-  app.use('/assets', express.static('public')); 
-  
-  app.get('/', (req, res) => {
-    res.render('index.ejs', {
-      user: req.session.user,
-    });
-  });
-  /*
-  app.use(passUserToView);
-  app.use('/auth', authController);
-  app.use(isSignedIn);
-  app.use('/users', usersController);
-  //app.use for other controllers 
+app.use(passUserToView);
+app.use('/auth', authController);
+app.use(isSignedIn);
+app.use('/users', usersController);
+app.use('/users/:userId/items', itemsController);
+app.use("/trips", tripsController);
+app.use("/lists", listsController);
 
-*/
-
-app.listen(3000, () => console.log('Running on port 3000! ')); 
+app.listen(port, () => {
+  console.log(`The express app is ready on port ${port}!`);
+});
